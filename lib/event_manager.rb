ff4533f0e6c98f826a78465e4ff0168e5d1cb61a
Rajesh Patra <p7.rajesh@gmail.com>
2015-09-03 18:22:06 +0530

    added csv library function in Ruby

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,16 +1,23 @@
+require "csv" #to load csv library in Ruby.
 puts "EventManager Initialized!"
 
-content = File.read "event_attendances.csv"
-puts content
+# content = File.read "event_attendances.csv"
+# puts content
 
-lines = File.readlines "event_attendances.csv"
+# Instead of read or readlines we use CSV’s open method to load our file.
+contents = CSV.open "event_attendances.csv", headers: true
+contents.each do |row|
+	name = row[2]
+	puts name
+end
+# lines = File.readlines "event_attendances.csv"
 
 # row_index = 0
 
 
 
 # each_with_index Calls block with two arguments, the item and its index, for each item in that array.
-lines.each_with_index do |line,index|
+# lines.each_with_index do |line,index|
 
 	# skipping header
 	# next if excutes like go to next, if (condition) = true
@@ -18,8 +25,8 @@ lines.each_with_index do |line,index|
 	
 	# row_index = row_index + 1
 
-	next if index == 0
-	columns = line.split(",")
-	name = columns[2]
-	puts name
-end
+	# next if index == 0
+	# columns = line.split(",")
+	# name = columns[2]
+	# puts name
+# end
