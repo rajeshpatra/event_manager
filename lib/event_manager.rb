695588415f2a9858d67ccd54aa30f2f6a29d1b84
Rajesh Patra <p7.rajesh@gmail.com>
2015-09-03 17:57:30 +0530

    added skipping the header with .each_with_index

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -5,15 +5,20 @@ puts content
 
 lines = File.readlines "event_attendances.csv"
 
-row_index = 0
-lines.each do |line|
+# row_index = 0
+
+
+
+# each_with_index Calls block with two arguments, the item and its index, for each item in that array.
+lines.each_with_index do |line,index|
 
 	# skipping header
 	# next if excutes like go to next, if (condition) = true
 	# next if line = " , RegDate, first_name, last_name, Email_Adress, HomePhone, Street, City, State, Zipcode\n"
 	
-	row_index = row_index + 1
-	next if row_index ==1	
+	# row_index = row_index + 1
+
+	next if index == 0
 	columns = line.split(",")
 	name = columns[2]
 	puts name
