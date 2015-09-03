62d2323ba560faf1c32c6a592166498b8bcd7b10
Rajesh Patra <p7.rajesh@gmail.com>
2015-09-03 17:44:19 +0530

    added skipping the header always

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -4,11 +4,16 @@ content = File.read "event_attendances.csv"
 puts content
 
 lines = File.readlines "event_attendances.csv"
+
+row_index = 0
 lines.each do |line|
 
 	# skipping header
 	# next if excutes like go to next, if (condition) = true
-	next if line = " , RegDate, first_name, last_name, Email_Adress, HomePhone, Street, City, State, Zipcode\n"
+	# next if line = " , RegDate, first_name, last_name, Email_Adress, HomePhone, Street, City, State, Zipcode\n"
+	
+	row_index = row_index + 1
+	next if row_index ==1	
 	columns = line.split(",")
 	name = columns[2]
 	puts name
